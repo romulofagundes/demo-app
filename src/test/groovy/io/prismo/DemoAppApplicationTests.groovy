de294644c17f9596f64dfeb242e44b0f1aac445c
Romulo Fagundes Cantanhede <romulo.fagundes@webonesystem.com.br>
2021-03-18 16:13:22 -0300

    init: commit inicial do projeto

---
+++ src/test/groovy/io/prismo/DemoAppApplicationTests.groovy
@@ -0,0 +1,13 @@
+package io.prismo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
